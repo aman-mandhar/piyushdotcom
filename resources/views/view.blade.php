1d19f816c5b6e40bf4f2ce45a5f865d5e79329a4
aman-mandhar <parthmultisolution@gmail.com>
2025-04-26 04:15:34 +0530

    1

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -137,7 +137,65 @@
                 <h5 class="mb-3">Contact Seller</h5>
                 <p><strong>Name:</strong> {{ $property->user->name }}</p>
                 <p><strong>City:</strong> {{ $property->user->city->name }}</p>
-
+                @php
+                    $user = Auth::user();
+                @endphp
+                @if($user == null)
+                    <h6>
+                        <div class="text-danger">Login to see the contact details</div>
+                    </h6>
+                    <div class="mt-3">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                        <span>New User <a href="{{ route('register') }}" class="btn btn-outline-primary w-100 mt-2">FREE SignUp!</a></span>
+                    </div>
+                @else
                 @auth
                     @php
                         $city_id = $property->city_id;
@@ -175,6 +233,7 @@
                         {{-- OR Option B: Modal trigger --}}
                         {{-- <a href="#" class="btn btn-success w-100 mt-2" data-bs-toggle="modal" data-bs-target="#authModal">Login to Contact</a> --}}
                 @endauth
+                @endif
             </div>
             <!-- Displaying the youtube video if available -->
             <div class="card p-4 mt-4">
