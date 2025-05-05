33f42496ea9615e1bc45aecfe9312bcce8267c46
aman-mandhar <parthmultisolution@gmail.com>
2025-05-05 10:29:56 +0530

    1

--- resources/views/components/register-modal.blade.php
+++ resources/views/components/register-modal.blade.php
@@ -1,8 +1,20 @@
 <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
+
             {{-- Livewire Register Component --}}
             @livewire('register')
+
+            {{-- Login Link in Footer --}}
+            <div class="modal-footer text-center d-block">
+                <small>
+                    Already have an account?
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal" data-bs-dismiss="modal">
+                        Login
+                    </a>
+                </small>
+            </div>
+
         </div>
     </div>
 </div>
